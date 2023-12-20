d0e7a5ec9961b34bd0d4169a394fe09ae71e556f
Apsingh <a.p.singh5508@gmail.com>
2023-12-20 22:00:33 +0530

    The Smallest Element in the Array

---
+++ Nqt.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+/**
+ * TCSNqt
+ */
+
+//  Find the smallest element in an array
+
+//  Example 1:
+//  Input: arr[] = {2,5,1,3,0};
+//  Output: 0
+//  Explanation: 0 is the smallest element in the array. 
+ 
+//  Example2: 
+//  Input: arr[] = {8,10,5,7,9};
+//  Output: 5
+//  Explanation: 5 is the smallest element in the array
+
+
+
+public class Nqt {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        
+        // n is the Size of the Array
+        int n=sc.nextInt();
+        int Arr[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            Arr[i]=sc.nextInt();
+        }
+
+        Arrays.sort(Arr);
+        int small=0;
+        small=Arr[0];
+        System.out.println(small+" is the smallest element in tha Arrays");
+    }
+}
+
+//By -- Akhand Pratap Singh..
